42aedfccd6d817c77549e69a221d05a6d960390c
Victor Lau <vtleluc@gmail.com>
2022-12-25 02:13:43 +0800

    init command setup

--- api/telegramWebhook.ts
+++ api/telegramWebhook.ts
@@ -1,13 +1,23 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
 import { Telegraf } from "telegraf";
 
 export const BOT_TOKEN = process.env.BOT_TOKEN
 const bot = new Telegraf(BOT_TOKEN as string)
 
-bot.start((ctx) => {
-  ctx.reply("Welcome")
+bot.start(async (ctx) => {
+  await ctx.reply("Welcome to timezone bot.")
 })
 
-export default async function handler(request: any, response: any) {
+bot.command("test",async (ctx) => {
+  await ctx.reply("test")
+})
+
+export default async function handler(request: VercelRequest, response: VercelResponse) {
   const { body, query } = request
-  bot.handleUpdate(body)
+  await bot.handleUpdate(body)
+  response.status(200).json({
+    body: request.body,
+    query: request.query,
+    cookies: request.cookies,
+  })
 }
